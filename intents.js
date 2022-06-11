3e5146d7117356c63aedd46b631bf64049571a69
NidhalWers <nteyeb75@gmail.com>
2022-06-11 19:17:57 +0200

    chatbot_v1

---
+++ intents.js
@@ -0,0 +1,75 @@
+data = {
+    "intents": [
+        {
+            "tag": "greeting",
+            "patterns": [
+                "Hello",
+                "Bonjour",
+                "Salam",
+                "yo",
+                "salut"
+            ],
+            "responses": [
+                "Bonjour",
+                "wa aleyk as salam wa rahmatulahi",
+                "hello",
+                "salutations !"
+            ]
+        },
+        {
+            "tag": "age",
+            "patterns": [
+                "Quel âge as-tu?",
+                "C'est quand ton anniversaire?",
+                "Quand es-tu né?"
+            ],
+            "responses": [
+                "J'ai 25 ans",
+                "Je suis né en 1996",
+                "Ma dated'anniversaire est le 3 juillet et je suis né en 1996",
+                "03/07/1996"
+            ]
+        },
+        {
+            "tag": "date",
+            "patterns": [
+                "Que fais-tu ce week-end?",
+                "Tu veux qu'on fasse un truc ensemble?",
+                "Quels sont tes plans pour cette semaine"
+            ],
+            "responses": [
+                "Je suis libre toute la semaine",
+                "Je n'ai rien de prévu",
+                "Je ne suis pas occupé"
+            ]
+        },
+        {
+            "tag": "name",
+            "patterns": [
+                "Quel est ton prenom?",
+                "Comment tu t'appelles?",
+                "Qui es-tu?"
+            ],
+            "responses": [
+                "Mon prénom est Miki",
+                "Je suis Miki",
+                "Miki"
+            ]
+        },
+        {
+            "tag": "goodbye",
+            "patterns": [
+                "bye",
+                "Salut",
+                "seenya",
+                "adios",
+                "cya"
+            ],
+            "responses": [
+                "C'était sympa de te parler",
+                "à plus tard",
+                "On se reparle tres vite!"
+            ]
+        }
+    ]
+}
